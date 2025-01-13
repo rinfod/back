a6dfb8e4ac1a5bb72287f5d8ca9715b6bcb5f692
rolando <rolando.info.des@gmail.com>
2025-01-13 11:58:39 -0600

    back

---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/obsano', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.log(err));
+
+// Modelo
+const ItemSchema = new mongoose.Schema({
+    region: { type: String, required: true },
+    distrito: { type: String },
+    descripcion: {type: String},
+});
+const Item = mongoose.model('Item', ItemSchema);
+
+// Rutas CRUD
+app.get('/items', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+app.post('/items', async (req, res) => {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.json(newItem);
+});
+
+app.put('/items/:id', async (req, res) => {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedItem);
+});
+
+app.delete('/items/:id', async (req, res) => {
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Item eliminado' });
+});
+
+app.listen(5000, () => console.log('Servidor corriendo en http://localhost:5000'));
